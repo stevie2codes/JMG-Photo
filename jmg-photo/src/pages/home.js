f35bbca28f9e2c295dfe2abbc1367422352e6e1d
stevie2codes <stevie2codes@gmail.com>
2020-04-26 00:00:28 -0500

    small change for deploy:

--- jmg-photo/src/pages/home.js
+++ jmg-photo/src/pages/home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div>
       <NavBar />
       <div class="wrapper">
-        <div class="box a">A</div>
+        <div class="box a">NEWBOX</div>
         <div class="box b">B</div>
         <div class="box c">C</div>
         <div class="box d">D</div>
