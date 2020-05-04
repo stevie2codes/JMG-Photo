4cf2cee1fd6830f53f6091c28967f5811cc59429
stevie2codes <stevie2codes@gmail.com>
2020-05-03 22:59:18 -0500

    changed carousel sizing

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,27 +4,27 @@ const Carousel = () => {
   return (
     <div
       id="carouselExampleInterval"
-      class="carousel slide d-flex m-auto"
+      className="carousel slide carousel-fade d-flex m-auto"
       data-ride="carousel"
     >
       <div className="carousel-inner">
-        <div className="carousel-item active" data-interval="10000">
+        <div className="carousel-item active" data-interval="3000">
           <img
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588174751/DSC_3731-Edit_ifqqg0.jpg"
-            class="d-block w-100 m-auto"
+            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562728/jg-photo/DSC_1874-Edit_vgltlt.jpg"
+            className="d-block w-100 m-auto"
             alt="landscape"
           ></img>
         </div>
-        <div className="carousel-item" data-interval="2000">
+        <div className="carousel-item" data-interval="3000">
           <img
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1587574800/green-road.jpg"
+            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562704/jg-photo/DSC_1655-Edit_xywign.jpg"
             className="d-block w-100"
             alt="..."
           ></img>
         </div>
-        <div className="carousel-item">
+        <div className="carousel-item" data-interval="3000">
           <img
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588137287/jg-photo/DSC_3309-Edit_aijgpi.jpg"
+            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562704/jg-photo/DSC_1489-Edit_x7vdrg.jpg"
             className="d-block w-100"
             alt="..."
           ></img>
