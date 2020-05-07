b3a669da36af89693a97530f013004d6d33137b3
stevie2codes <stevie2codes@gmail.com>
2020-05-07 13:45:56 -0500

    setup routes and hover effects on homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import React from "react";
 import "../sass/home.scss";
 // import HomeCube from "../components/three";
 // import Carousel from "../components/carousel";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
@@ -11,34 +11,54 @@ const Home = () => {
         <h1 className="sectionName text-center m-5">Home</h1>
       </div>
       <div className="card-columns home">
-        <div className="card">
-          <img
-            className="card-img-top"
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588176708/jg_wkh7ap.jpg"
-            alt="horse"
-          />
-        </div>
-        <div className="card">
-          <img
-            className="card-img-top"
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562700/jg-photo/DSC_0375_rr5fng.jpg"
-            alt="horse"
-          />
-        </div>
-        <div className="card">
-          <img
-            className="card-img-top"
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562703/jg-photo/DSC_1066_vnqnr7.jpg"
-            alt="horse"
-          />
-        </div>
-        <div className="card">
-          <img
-            className="card-img-top"
-            src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562700/jg-photo/DSC_0375_rr5fng.jpg"
-            alt="horse"
-          />
-        </div>
+        <Link to="/portraits">
+          <div className="card portraitCard hvrbox">
+            <img
+              className="card-img-top hvrbox-layer_bottom"
+              src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588176708/jg_wkh7ap.jpg"
+              alt="portraits"
+            />{" "}
+            <div className="hvrbox-layer_top hvrbox-layer_slideup">
+              <div className="hvrbox-text">Portraits</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/furryFriends">
+          <div className="card portraitCard hvrbox">
+            <img
+              className="card-img-top hvrbox-layer_bottom"
+              src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562700/jg-photo/DSC_0375_rr5fng.jpg"
+              alt="portraits"
+            />{" "}
+            <div className="hvrbox-layer_top hvrbox-layer_slideup">
+              <div className="hvrbox-text">Furry Friends</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/landscapes">
+          <div className="card portraitCard hvrbox">
+            <img
+              className="card-img-top hvrbox-layer_bottom"
+              src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588562746/jg-photo/DSC_3309-Edit_ijb7hz.jpg"
+              alt="portraits"
+            />{" "}
+            <div className="hvrbox-layer_top hvrbox-layer_slideup">
+              <div className="hvrbox-text">Landscapes</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/weddings">
+          <div className="card portraitCard hvrbox">
+            <img
+              className="card-img-top hvrbox-layer_bottom"
+              src="https://res.cloudinary.com/df9q0hnuw/image/upload/v1588176708/jg_wkh7ap.jpg"
+              alt="portraits"
+            />{" "}
+            <div className="hvrbox-layer_top hvrbox-layer_slideup">
+              <div className="hvrbox-text">Weddings</div>
+            </div>
+          </div>
+        </Link>
       </div>
       {/* <HomeCube /> */}
       {/* <Carousel /> */}
