0d07e01aed75f82dee2f38b4512e458af74c1b3a
stevie2codes <stevie2codes@gmail.com>
2020-05-11 15:15:48 -0500

    uploaded all portrait photos

--- src/pages/portraits.js
+++ src/pages/portraits.js
@@ -1,11 +1,66 @@
 import React from "react";
+import "../sass/furryFriends.scss";
+import ReactImageAppear from "react-image-appear";
+import { getPorts } from "../services/ports";
+import Pagination from "../common/pagination";
+import { paginate } from "../utils/paginate";
 
-const Portraits = () => {
-  return (
-    <div className="sectionDiv">
-      <h1 className="sectionName text-center m-5">portraits</h1>
-    </div>
-  );
-};
+class Portraits extends React.Component {
+  state = {
+    ports: getPorts(),
+    loading: true,
+    currentPage: 1,
+    pageSize: 9
+  };
 
+  componentDidMount() {
+    this.setState({
+      loading: false
+    });
+  }
+  addDefaultSrc = ev => {
+    ev.target.src = "https://www.stevensegallery.com/640/360";
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    const { pageSize, currentPage, loading, ports } = this.state;
+
+    if (loading) {
+      return <h1>loading … </h1>;
+    }
+    let images = paginate(ports, currentPage, pageSize);
+    return (
+      <React.Fragment>
+        {" "}
+        <div className="sectionDiv">
+          <h1 className="sectionName text-center m-5">Portraits</h1>
+        </div>
+        <div className="card-columns pups">
+          {images.map(img => (
+            <div className="card" key={img.id}>
+              <ReactImageAppear
+                showLoader={false}
+                easing="ease-in"
+                src={img.image}
+                className="card-img-top"
+                onError={this.addDefaultSrc}
+                alt="Portraitss"
+              />
+            </div>
+          ))}
+        </div>
+        <Pagination
+          itemsCount={ports.length}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
+      </React.Fragment>
+    );
+  }
+}
 export default Portraits;
---
+++ src/services/ports.js
@@ -0,0 +1,128 @@
+const portraits = [
+  {
+    id: 1,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_7010_dacotm.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 2,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_8551-Edit_rklgtv.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 3,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227153/jg-photo/portraits/DSC_8202-Edit-2_r1yfhh.jpg`,
+    type: "portraits"
+  },
+
+  {
+    id: 4,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_5074-Edit_thdgol.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 5,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_5097-Edit_co11g2.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 6,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_7138-Edit-Edit_pic633.jpg`,
+    type: "portraits"
+  },
+
+  {
+    id: 7,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_5098-Edit_bgql8v.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 8,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227152/jg-photo/portraits/DSC_7137-Edit-Edit_w04po6.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 9,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227127/jg-photo/portraits/DSC_4989_gg2gbv.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 10,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227127/jg-photo/portraits/DSC_4198-Edit-Edit_hjgx6n.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 11,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_1589_q3ogrl.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 12,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_4106-Edit_pqcctu.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 13,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_4183-Edit-Edit_iliiqq.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 14,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_1467_tkxkum.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 15,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_1610_tgzqjq.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 16,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/DSC_1481_y9zowy.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 17,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227126/jg-photo/portraits/DSC_1454_hzmba2.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 18,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/DSC_1476_kmavlv.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 19,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/48428153_2046293532328334_1483581055221891072_o_fqrxqc.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 20,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/23826054_1815779495379740_6836805092082104316_o_dwcgsw.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 21,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/48396492_2046293542328333_5011006801726930944_o_korzxh.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 22,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/46706082_2026982974259390_6904359665774100480_o_nxnaka.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 23,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227125/jg-photo/portraits/48379376_2046301285660892_9003800028644376576_o_tl8rfd.jpg`,
+    type: "portraits"
+  },
+  {
+    id: 24,
+    image: `https://res.cloudinary.com/df9q0hnuw/image/upload/v1589227124/jg-photo/portraits/46502112_2026983007592720_1997143016497741824_o_xjy8lu.jpg`,
+    type: "portraits"
+  }
+];
+
+export function getPorts() {
+  return portraits;
+}
