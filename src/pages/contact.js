78421877accbab8980764e09b1e2d094571389bc
stevie2codes <stevie2codes@gmail.com>
2020-05-11 15:59:36 -0500

    setup contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "../sass/contact.scss";
 
 const Contact = () => {
   return (
-    <div className="sectionDiv">
-      <h1 className="sectionName text-center m-5">Contact</h1>
-    </div>
+    <React.Fragment>
+      <div className="sectionDiv">
+        <h1 className="sectionName text-center m-5">Contact</h1>
+      </div>
+      <div className="contactCard">
+        <i className="fas fa-at">
+          {" "}
+          <p>Email: placeholer.com</p>
+        </i>
+        <i className="fab fa-instagram">
+          {" "}
+          <p>Instagram</p>
+        </i>
+        <i className="fab fa-linkedin">
+          <p>LinkedIn</p>
+        </i>{" "}
+      </div>
+    </React.Fragment>
   );
 };
 
